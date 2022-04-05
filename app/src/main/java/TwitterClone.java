0ff9bac20f2cc7efc21dc54526dc6ad1172f1aba
123 <zarifkhakimov0722@gmail.com>
2022-04-05 16:40:20 +0500

    Changed ImageView

---
+++ app/src/main/java/TwitterClone.java
@@ -0,0 +1,15 @@
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myjavaapplication.R;
+
+public class TwitterClone extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.twitter_clone);
+
+    }
+}
---
+++ app/src/main/java/com/example/myjavaapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myjavaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Object TwitterClone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // My third project in GitHub. Let's go
+    }
+}
